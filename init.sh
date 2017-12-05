5450f6a93cb4ca18e921639b5d7d38a3d59625c4
Nasjalnika <load.lin@yahoo.com>
2017-12-05 21:01:27 +0300

    www

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
---
+++ init_start.sh
@@ -0,0 +1,6 @@
+mkdir /home/box/web
+mkdir /home/box/web/public
+mkdir /home/box/web/public/img
+mkdir /home/box/web/public/js
+mkdir /home/box/web/uploads
+mkdir /home/box/web/etc
