90b0db1781b686a0d9c061875e2ab9c2325f8f9e
Nasjalnika <load.lin@yahoo.com>
2017-12-06 22:42:00 +0300

    2

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
