88d7d753bc30fc7c2676081ae60c91b62ca0c97b
Nasjalnika <load.lin@yahoo.com>
2017-12-13 21:18:59 +0300

    gunicorn added

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/gunicorn.conf
+sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
 
 
