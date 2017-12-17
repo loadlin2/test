8e070296ca45bd4f19cbb50ae50549fc969d43a6
Nasjalnika <load.lin@yahoo.com>
2017-12-17 18:01:27 +0300

    gun9

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ sudo chmod 777 /home/box/web/
 sudo chmod 777 /home/box/web/*
 
 sudo /etc/init.d/nginx restart
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello
 #sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/gunicorn.conf
