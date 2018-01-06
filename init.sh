3c3d08224803335d8a22c50bc4d97def38a321ce
Nasjalnika <load.lin@yahoo.com>
2018-01-06 22:51:53 +0300

    Django + gunicorn
    first iteration
    `

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ sudo /etc/init.d/nginx restart
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello
+sudo ln -s /home/box/web/etc/gunidjanga.conf   /etc/gunicorn.d/gunidjanga
 #sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
